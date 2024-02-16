c379978c22ff312ee6ce2990204f5b43b9b918e2
AndreyBachinskye <126277911+AndreyBachinskye@users.noreply.github.com>
2024-02-16 13:07:52 +0200

    try 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import css from './app.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmitData = data => {
+    const ifSome = this.state.contacts.some(
+      name => name.name.toLowerCase() === data.name.toLowerCase()
+    );
+    if (ifSome) {
+      alert(`The contact with name "${data.name}" is already aded`);
+      return;
+    }
+    this.setState(prev => ({
+      contacts: [...prev.contacts, data],
+    }));
+  };
+
+  onDeleteItem = name => {
+    this.setState({
+      contacts: this.state.contacts.filter(item => item.name !== name),
+    });
+  };
+
+  onFilterChange = evt => {
+    this.setState({
+      filter: evt.currentTarget.value,
+    });
+  };
+
+  getFilteredContacts = () => {
+    return this.state.contacts.filter(el =>
+      el.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+  };
+
+  render() {
+    return (
+      <div className={css.div}>
+        <h1>Phonebook</h1>
+        <ContactForm submitHandler={this.formSubmitData} />
+
+        <h2>Contacts</h2>
+        <Filter
+          onFilterChange={this.onFilterChange}
+          filterInput={this.state.filter}
+        />
+        <ContactList
+          filteredContacts={this.getFilteredContacts()}
+          onDeleteItem={this.onDeleteItem}
+        />
+      </div>
+    );
+  }
+}
